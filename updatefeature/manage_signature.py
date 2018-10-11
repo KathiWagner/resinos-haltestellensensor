5ab32e2cc68f6c85d1e1fee4f780a4e2c406ca31
von Schwerin <cschwerin@hs-ulm.de>
2018-10-11 14:51:43 +0200

    Signature manager added, run_update_on_exisiting.py updated.

---
+++ updatefeature/manage_signature.py
@@ -0,0 +1,90 @@
+import sys
+import argparse
+import binascii
+import hashlib
+import base64
+from Crypto import Random
+from Crypto.Cipher import AES
+
+class AESCipher(object):
+
+    def __init__(self, key):
+        self.bs = 32
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+def get_raw_signature(key, file):
+    sha512obj = hashlib.sha512()
+    with open(file, 'r') as reader:
+        chunk = reader.read(52428800)  # Read 50MB
+        while len(chunk) == 52428800:
+            sha512obj.update(chunk)
+            chunk = reader.read(52428800)  # Read 50MB
+        if len(chunk) > 0:
+            sha512obj.update(chunk)
+    if sha512obj.digest_size > 0:
+        return sha512obj.hexdigest()
+    else:
+        raise AssertionError('Sha512 digest size for input file is 0')
+
+def sign(key, file, signaturefile):
+        raw_signature = get_raw_signature(key, file)
+        cipher = AESCipher(key)
+        signature = cipher.encrypt(raw_signature)
+        with open(signaturefile, 'w') as writer:
+            writer.write(signature)
+
+def check(key, file, signaturefile):
+    raw_signature = get_raw_signature(key, file)
+    cipher = AESCipher(key)
+    with open(signaturefile, 'r') as reader:
+        foreign_sig = reader.read().rstrip()
+        if cipher.decrypt(foreign_sig) == raw_signature:
+            return True
+    return False
+
+if __name__=="__main__":
+    ap = argparse.ArgumentParser(description="Create or check file signature (sha512 with AES in ECB mode)")
+    ap.add_argument('-k', dest='key', help='The cryptographic key')
+    ap.add_argument('-f', dest='file', help='The signed file')
+    ap.add_argument('-s', dest='sig_file', help='The signature file')
+    ap.add_argument('--silent', dest='silent', help='Do only print the result', action='store_true')
+    ap.add_argument('--sign', help='Sign the given file', action="store_true")
+    ap.add_argument('--check', help='Check the ', action="store_true")
+    args = ap.parse_args()
+    #binkey = bytearray(args.key.encode('ascii'))
+    #if not args.silent:
+    #    print('Key: ' + binascii.hexlify(binkey).decode('ascii'))
+    if args.check and not args.sign:
+        if check(args.key, args.file, args.sig_file):
+            print('Match')
+            sys.exit(0)
+        else:
+            print('No match')
+            sys.exit(1)
+
+    elif args.sign:
+        sign(args.key, args.file, args.sig_file)
+        if not args.silent:
+            print('Successfully created signature in file ' + args.sig_file)
+        sys.exit(0)
+
+
--- updatefeature/run_update_on_existing.py
+++ updatefeature/run_update_on_existing.py
@@ -7,42 +7,87 @@ import time
 
 params = {}
 params['Name'] = 'hss-app-2018-09-21.7z'
+if len(sys.argv) > 1:
+    params['Name'] = sys.argv[1]
 updatedir = '/home/pi/updates'
 updatefilepath = os.path.join(updatedir, params['Name'])
 
+#Log options - file or stderr
+logopt = 'file'
+loghandle = None
+
+
+def get_log_handle():
+    global loghandle
+    if not loghandle:
+        if logopt == 'file':
+            loghandle = open('/home/pi/updates/update.log', 'a')
+        else:
+            loghandle = sys.stderr
+    return loghandle
+
+
+def log(logstr):
+    get_log_handle().write(logstr)
+
+
+def close_and_exit(status):
+    log('Exiting with code ' + str(status) + '\n')
+    try:
+        loghandle.close()
+    except:
+        pass
+    sys.exit(status)
+
 # Run update process
-sys.stderr.write('Update file received. Starting update...\n')
+log('Update file received. Starting update...\n')
 output = ''
 imgtarname = params['Name'][:params['Name'].rfind('.')] + '.tar'
 imgtarpath = os.path.join(updatedir, imgtarname)
-sp.call(['p7zip', '-d', '-k', '-f', params['Name']], cwd=updatedir, stdout=sys.stderr)
-sp.call(['balena', 'load', '-i', imgtarname], cwd=updatedir, stdout=sys.stderr)
-sp.call(['balena', 'stop', 'hss-app-container'], cwd=updatedir, stdout=sys.stderr)
-sys.stderr.write("Waiting for container to start up again...")
-time.sleep(15)
-output = sp.check_output(['balena', 'container', 'ls'], cwd=updatedir).decode('ascii')
-sys.stderr.write('Running containers: ' + output)
+try:
+    output = sp.check_output(['p7zip', '-d', '-k', '-f', params['Name']], cwd=updatedir, stderr=sp.STDOUT).decode(
+        'ascii')
+    log(output)
+except sp.CalledProcessError as ex:
+    log(str(ex))
+    close_and_exit(1)
+output = sp.check_output(['balena', 'load', '-i', imgtarname], cwd=updatedir, stderr=sp.STDOUT).decode('ascii')
+if output.find('no such file or directory') < 0:
+    log("Image loaded successfully! Restarting container...\n")
+    try:
+        output = sp.check_output(['balena', 'stop', 'hss-app-container'], cwd=updatedir, stderr=sp.STDOUT).decode(
+            'ascii')
+        log(output)
+    except sp.CalledProcessError as ex:
+        log(str(ex))
+        close_and_exit(1)
+    log("Waiting for container to start up again...\n")
+    time.sleep(15)
+    output = sp.check_output(['balena', 'container', 'ls'], cwd=updatedir, stderr=sp.STDOUT).decode('ascii')
+    log('Running containers: ' + output + '\n')
+else:
+    output = ''
+    log("Loading image failed!\n")
 if output.find('hss-app-container') > 0:
-    sys.stderr.write('Update successful! Cleaning up...\n')
+    log('Update successful! Cleaning up...\n')
     try:
         if os.path.exists(updatefilepath):
             os.remove(updatefilepath)
         if os.path.exists(imgtarpath):
             os.remove(imgtarpath)
         status = sp.call(['balena', 'image', 'prune', '-f'])
-        if status == 0:
-            sys.stderr.write('Cleanup finished! Exiting...\n')
-            sys.exit(0)
+        log('Cleanup finished! Exiting...\n')
+        close_and_exit(0)
     except:
         pass
-    sys.stderr.write('Cleanup could not be finished. Please clean up manually! Exiting...\n')
-    sys.exit(0)
+    log('Cleanup could not be finished. Please clean up manually! Exiting...\n')
+    close_and_exit(0)
 
 else:
-    sys.stderr.write('Update unsuccessful. Checking status of old container...')
+    log('Update unsuccessful. Checking status of old container...')
     output = sp.check_output(['balena', 'container', 'ls'], cwd=updatedir).decode('ascii')
     if output.find('hss-app-container') > 0:
-        sys.stderr.write('Old container still up and running! Exiting...')
+        log('Old container still up and running! Exiting...')
         sys.exit(1)
-    sys.stderr.write('Old container not up and running! Please revert manually! Exiting...\n')
-    sys.exit(1)
+    log('Old container not up and running! Please revert manually! Exiting...\n')
+    close_and_exit(1)
